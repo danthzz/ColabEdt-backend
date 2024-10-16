5e7c306e8c8c0985235cec7a756eb204b781209c
Daniel Gomes <danieflam3@gmail.com>
2024-10-16 14:39:54 -0300

    cors config

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIO(server, {
   cors: {
-    origin: true,
+    origin: '*',
     methods: ['GET', 'POST', 'PUT'],
     credentials: true,
   },
